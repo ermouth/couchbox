db7ba3c3253fbfc5cc05212779937b1ee6e42fbd
Anton V <dasiderk@gmail.com>
2017-02-17 10:39:09 +0000

    fix fetch setting node address

--- methods/fetch.js
+++ methods/fetch.js
@@ -4,12 +4,14 @@ const fetch = require('node-fetch');
 const couchdb = require('../utils/couchdb');
 const config = require('../config');
 
-
+const NODE_NAME = config.get('couchbox.nodename');
+const NODE_URL = config.get('couchdb.connection') +'://'+ config.get('couchdb.ip') +':'+ config.get('couchdb.port'); 
 const HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD'];
 const HTTP_METHODS_VALID = {};
 HTTP_METHODS.forEach(m => HTTP_METHODS_VALID[m] = true);
-
-const getNodeURL = (node) => config.get('nodes.domainPrefix') + node +'.'+ config.get('nodes.domain');
+const getNodeURL = (node) => NODE_NAME === node
+    ? NODE_URL
+    : config.get('nodes.domainPrefix') + node +'.'+ config.get('nodes.domain');
 
 module.exports = function () {
   const options = Object.isObject(arguments[0]) ? arguments[0] : {};
@@ -31,7 +33,11 @@ module.exports = function () {
     const nodeURL = getNodeURL(options.node);
     if (!nodeURL) return Promise.reject(new Error('Bad node: '+ options.node));
     if (/^https?:/.test(url)) return Promise.reject(new Error('Bad url: '+ url));
-    url = getNodeURL(options.node) + (url[0] === '/' ? '' : '/') + url;
+    url = nodeURL + (url[0] === '/' ? '' : '/') + url;
+  } else {
+    if (!/^https?:/.test(url)) {
+      url = NODE_URL + (url[0] === '/' ? '' : '/') + url;
+    }
   }
 
   if (options.body) {
