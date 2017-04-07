f64debd8fb2b609c8643c95f378c234555372ceb
Anton Veshnyakov <ftescht@fastmail.com>
2017-04-07 14:46:59 +0300

    lib refactor

--- utils/lib.js
+++ utils/lib.js
@@ -2,19 +2,21 @@ require('sugar');
 const crypto = require('crypto');
 const globalsDetect = require('acorn-globals');
 
-const uuid = module.exports.uuid = (now = Date.now()) => (now+'').substr(0,12)+('0000'+Number.random(46656,2821109907455).toString(36)).substr(-8);
 
-const sdbmCode = module.exports.sdbmCode = function sdbmCode(src) {
-  // Very fast hash used in Berkeley DB
-  const s = JSON.stringify(src), length = s.length;
-  let i = 0, hash = -219;
-  while (i < length) hash = s.charCodeAt(i++)+(hash<<6)+(hash<<16)-hash;
-  return (1e11+hash).toString(36);
-};
+// Atomics
 
-const hashMD5 = module.exports.hashMD5 = (data) => crypto.createHash('md5').update(JSON.stringify(data)).digest('hex');
+function notEmpty(val) {
+  return val === undefined || val === null;
+}
 
-const parseJSON = module.exports.parseJSON = function parseJSON(json) {
+function isEmpty(val) {
+  return !notEmpty(val);
+}
+
+
+// Utils
+
+function parseJSON(json) {
   let result;
   try {
     result = JSON.parse(json);
@@ -22,15 +24,84 @@ const parseJSON = module.exports.parseJSON = function parseJSON(json) {
     result = undefined;
   }
   return result;
+}
+
+function toBase64(str) {
+  return new Buffer(str).toString('base64');
+}
+
+function coverFunction(funcSrc) {
+  return '(' + funcSrc + ')';
+}
+
+function evalFunc(funcSrc) {
+  return eval(coverFunction(funcSrc));
+}
+
+const addField = function addField(obj = {}, path, value, separator = '.') {
+  const fieldPath = Object.isString(path) ? path.split(separator) : path;
+  const field = fieldPath.shift();
+  if (field) {
+    obj[field] = fieldPath.length > 0
+      ? addField(obj[field], fieldPath, value, separator)
+      : value;
+  }
+  return obj;
 };
 
-const coverFunction = (funcSrc) => '(' + funcSrc + ')';
+const getField = function getField(obj = {}, path, separator = '.') {
+  const fieldPath = Object.isString(path) ? path.split(separator) : path;
+  const field = fieldPath.shift();
+  if (field) {
+    return fieldPath.length > 0
+      ? getField(obj[field], fieldPath, separator)
+      : obj[field];
+  }
+  return null;
+};
 
-const getGlobals = module.exports.getGlobals = function getGlobals(src) {
+
+// Hashes
+
+function uuid(now = Date.now()) {
+  return (now+'').substr(0,12)+('0000'+Number.random(46656,2821109907455).toString(36)).substr(-8);
+}
+
+function guid() {
+  const S4 = () => (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
+
+function sdbmCode(src) {
+  // Very fast hash used in Berkeley DB
+  const s = JSON.stringify(src), length = s.length;
+  let i = 0, hash = -219;
+  while (i < length) hash = s.charCodeAt(i++)+(hash<<6)+(hash<<16)-hash;
+  return (1e11+hash).toString(36);
+}
+
+function hashMD5(data) {
+  return crypto.createHash('md5').update(JSON.stringify(data)).digest('hex');
+}
+
+
+// Validators
+
+function checkPhone(phone) {
+  if (!phone || phone.length < 9) return null;
+  try {
+    phone = phone.slice(0, 30).replace(/\D/g, '');
+    if (/^(7|8)9\d{9}$/.test(phone)) return phone.slice(1);
+    if (/^9\d{9}$/.test(phone)) return phone;
+  } catch (e) { }
+  return null;
+}
+
+function getGlobals(src) {
   return (src) ? globalsDetect(src).map(i => i.name) : null;
-};
+}
 
-const validateGlobals = module.exports.validateGlobals = function validateGlobals(globals, params = {}) {
+function validateGlobals(globals, params = {}) {
   const errors = [];
   if (params.available && params.available.length) {
     const available = {};
@@ -50,47 +121,12 @@ const validateGlobals = module.exports.validateGlobals = function validateGlobal
     }
   }
   return errors.length ? errors : false;
-};
+}
 
-const getField = module.exports.getField = (obj = {}, path, separator = '.') => {
-  const fieldPath = Object.isString(path) ? path.split(separator) : path;
-  const field = fieldPath.shift();
-  if (field) {
-    return fieldPath.length > 0
-      ? getField(obj[field], fieldPath, separator)
-      : obj[field];
-  }
-  return null;
-};
 
-const addField = module.exports.addField = (obj = {}, path, value, separator = '.') => {
-  const fieldPath = Object.isString(path) ? path.split(separator) : path;
-  const field = fieldPath.shift();
-  if (field) {
-    obj[field] = fieldPath.length > 0
-      ? addField(obj[field], fieldPath, value, separator)
-      : value;
-  }
-  return obj;
-};
+// Errors
 
-const evalFunc = module.exports.evalFunc = (funcSrc) => eval(coverFunction(funcSrc));
-
-const checkPhone = module.exports.checkPhone = function checkPhone(phone) {
-  if (!phone || phone.length < 9) return null;
-  try {
-    phone = phone.slice(0, 30).replace(/\D/g, '');
-    if (/^(7|8)9\d{9}$/.test(phone)) return phone.slice(1);
-    if (/^9\d{9}$/.test(phone)) return phone;
-  } catch (e) { }
-  return null;
-};
-
-const toBase64 = module.exports.toBase64 = function toBase64(str) {
-  return new Buffer(str).toString('base64');
-};
-
-module.exports.errorBeautify = function errorStackGrabber(error) {
+function errorBeautify(error) {
   if (!error) return error;
   switch (error.message) {
     case 'operation timed out':
@@ -111,4 +147,28 @@ module.exports.errorBeautify = function errorStackGrabber(error) {
     }
   }
   return error;
+}
+
+module.exports = {
+  notEmpty,
+  isEmpty,
+
+  parseJSON,
+  toBase64,
+  coverFunction,
+  evalFunc,
+
+  uuid,
+  guid,
+  sdbmCode,
+  hashMD5,
+
+  getField,
+  addField,
+
+  checkPhone,
+  getGlobals,
+  validateGlobals,
+
+  errorBeautify
 };
