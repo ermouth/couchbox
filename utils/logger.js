ff43f3a9006d11aa89c15ca3be2c8aed1f82f42f
Anton Veshnyakov <ftescht@fastmail.com>
2017-04-07 19:27:54 +0300

    logger sendmail

--- utils/logger.js
+++ utils/logger.js
@@ -63,7 +63,7 @@ const sendMail = (recipients = config.get('couchbox.mail.recipients'), mailMessa
     '',
     mailMessage
   ].join('\n');
-  console.log('recipients', recipients);
+  console.log('recipients', config.get('couchbox.mail'));
   return execBash('printf "${mailMessage}" | sendmail "$recipients"', {
     mailMessage,
     recipients
