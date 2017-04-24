6cfb92380d7b48336fca6f11661ca8503bb38a1e
Anton Veshnyakov <ftescht@fastmail.com>
2017-04-24 13:57:35 +0300

    cleanup old code

--- workers/api/constants.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  API_URL_ROOT: '/',
-  API_URL_PREFIX: '_',
-
-  API_DEFAULT_TIMEOUT: 10e3,
-  API_DEFAULT_CODE: 200,
-  API_DEFAULT_HEADERS: { 'Content-Type': 'text/plain' }
-};
--- workers/socket/constants.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  SOCKET_NODE_DELIMITER: ':',
-  SOCKET_EVENT_PREFIX: 'socket.emit.'
-};
