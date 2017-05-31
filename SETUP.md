8c18b8720f71c73b455ad85c319e8ab65a0f3b96
Anton Veshnyakov <ftescht@fastmail.com>
2017-05-31 18:18:58 +0300

    setup.js refactoring

--- SETUP.md
+++ SETUP.md
@@ -1,15 +1,19 @@
 Couchbox installer
 ==================
 
-    couchbox/setup.js couchbox.json -n cb -A 127.0.0.1 -P 5984 -u couchbox -p couchbox -c http://localhost,http://127.0.0.1 -s 12345678901234567890123456789012
+    couchbox/setup.js couchbox.json -D testdb=testdoc.json -n cb -A 127.0.0.1 -P 5984 -u couchbox -p couchbox -c http://localhost,http://127.0.0.1 -s 12345678901234567890123456789012
 
+* `first arg` couchbox.json file path
+* `-D` paths to json type documents that need to save in couchdb (use `,` delimiter)
 * `-n` Node name
 * `-A` Couchdb ip address
 * `-P` Couchdb port
 * `-u` Couchdb user
 * `-p` Couchdb user password
-* `-c` set Couchdb cors in config
+* `-c` set Couchdb cors in config (use `,` delimiter)
 * `-s` set Couchdb secret key in config
 * `-r` Redis password
 * `-m` installer mode 'o' - overwrite 'p' - patch
+
+Document file name pattern `DBNAME=DOCNAME.json'
 
--- setup.js
+++ setup.js
@@ -6,11 +6,54 @@ const crypto = require('crypto');
 const fs = require('fs');
 const fetch = require('node-fetch');
 
+
 const isS = Object.isString;
 const isO = Object.isObject;
 const isA = Object.isArray;
 const isB = Object.isBoolean;
 const isN = Object.isNumber;
+
+
+// load json file
+const loadJSON = (filePath) => new Promise((resolve, reject) => {
+  if (!(filePath && isS(filePath))) return {};
+  fs.stat(filePath, (errorCheck) => {
+    if (errorCheck) return reject(errorCheck);
+    fs.readFile(filePath, (errorLoad, res) => {
+      if (errorLoad) return reject(errorLoad);
+      if (!res) return reject(new Error('No file'));
+      let json;
+      try {
+        json = JSON.parse(res.toString());
+      }
+      catch (errorParse) {
+        return reject(errorParse);
+      }
+      if (json && isO(json)) {
+        return resolve(json);
+      }
+      return reject(new Error('Bad json'));
+    });
+  });
+});
+
+// load couchbox.json
+const getConfigFile = loadJSON;
+
+// load doc.json
+const getDoc = (filePath) => loadJSON(filePath).then(json => {
+  let i = filePath.lastIndexOf('/');
+  if (i >= 0) filePath = filePath.substr(i + 1, filePath.length - i - 1).replace('.json', '');
+  i = filePath.indexOf('=');
+  filePath = [filePath.substr(0, i), filePath.substr(i + 1).replace('-', '/')];
+  return [filePath, json];
+});
+
+// load list of docs
+const getDocs = (docs) => {
+  if (!(docs && isA(docs) && docs.length > 0)) return Promise.resolve([]);
+  return Promise.map(docs, getDoc);
+};
 
 
 const checkAddress = (() => {
@@ -42,7 +85,7 @@ const toMode = mode => {
 };
 
 const CONFIG_PATH   = isS(argv._[0]) ? argv._[0] : null;
-const DDOCS         = isS(argv.D) ? argv.D.split(',') : [];
+const DOCS         = isS(argv.D) ? argv.D.split(',') : [];
 const NODE_NAME     = isS(argv.n) ? argv.n : null;
 const COUCHDB_USER  = isS(argv.u) ? argv.u : null;
 const COUCHDB_PASS  = isS(argv.p) ? argv.p : null;
@@ -56,106 +99,88 @@ const MODE          = isS(argv.m) ? toMode(argv.m) : null;
 const COUCHDB_URL   = 'http://'+ COUCHDB_USER +':'+ COUCHDB_PASS +'@'+ COUCHDB_IP +':'+ COUCHDB_PORT;
 
 
-const dbQuery = (path) => fetch(COUCHDB_URL + path, {
-  method: 'GET',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json; charset=utf-8',
-  }
-}).then(res => res.json());
+// query to db
+const dbQuery = (path, method = 'GET', body) => {
+  const query = {
+    method,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json; charset=utf-8',
+    }
+  };
+  if (body) query.body = JSON.stringify(body);
+  const url = COUCHDB_URL + (path[0] === '/' ? path : '/' + path);
+  return fetch(url, query).then(res => method === 'HEAD' ? res : res.json());
+};
 
+// remove item form couchdb config
 const removeConfigItem = (path) => {
-  console.log('remove config item', path);
-  return fetch(COUCHDB_URL + '/_config/' + encodeURI(path), {
-    method: 'DELETE',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-  }).then(res => res.json())
-    .then(res => {
-      if (res && res.error && res.reason) {
-        throw new Error('Error save '+ path +' ' + res.reason);
-      }
-      return [path, res];
-    });
+  console.log('Remove config item');
+  console.log('>', path);
+
+  return dbQuery('_config/' + encodeURI(path), 'DELETE').then(res => {
+    if (res && res.error && res.reason) {
+      throw new Error('Error remove '+ path +' ' + res.reason);
+    }
+    return ['REMOVED config item "' + path + '"', res];
+  });
 };
 
+// save item to couchdb config
 const saveConfigItem = (path, val) => {
-  console.log('save config item', path, val);
-  if (isB(val) || isN(val)) {
-    val = val.toString();
-  }
-  return fetch(COUCHDB_URL + '/_config/' + encodeURI(path), {
-    method: 'PUT',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    body: JSON.stringify(val)
-  }).then(res => res.json())
-    .then(res => {
-      if (res && res.error && res.reason) {
-        throw new Error('Error save '+ path +' ' + res.reason);
-      }
-      return [path, res];
-    });
+  console.log('Save config item');
+  console.log('>', path, val);
+
+  if (isB(val) || isN(val)) val = val.toString();
+
+  return dbQuery('_config/' + encodeURI(path), 'PUT', val).then(res => {
+    if (res && res.error && res.reason) {
+      throw new Error('Error save '+ path +' ' + res.reason);
+    }
+    return ['SAVED config item "' + path + '"', (res || val)];
+  });
 };
 
-const getConfigFile = (filePath) => new Promise((resolve, reject) => {
-  if (!(filePath && isS(filePath))) return {};
-  fs.stat(filePath, (errorCheck) => {
-    if (errorCheck) return reject(errorCheck);
-    fs.readFile(filePath, (errorLoad, res) => {
-      if (errorLoad) return reject(errorLoad);
-      if (!res) return reject(new Error('No file'));
-      let json;
-      try {
-        json = JSON.parse(res.toString());
-      }
-      catch (errorParse) {
-        return reject(errorParse);
-      }
-      if (json && isO(json)) {
-        return resolve(json);
-      }
-      return reject(new Error('Bad json'));
-    });
+// check db on exists
+const checkDB = (dbName) => {
+  console.log('Check db on exists');
+  console.log('>', dbName);
+
+  return dbQuery(dbName, 'HEAD').then(res => {
+    if (res && res.status === 200) return true;
+    throw new Error('No db '+ dbName);
   });
-});
-
-
-const getDDoc = (filePath) => new Promise((resolve, reject) => {
-  if (!(filePath && isS(filePath))) return reject(new Error('Bad file path'));
-  fs.stat(filePath, (errorCheck) => {
-    if (errorCheck) return reject(errorCheck);
-    fs.readFile(filePath, (errorLoad, res) => {
-      if (errorLoad) return reject(errorLoad);
-      if (!res) return reject(new Error('No file'));
-      let json;
-      try {
-        json = JSON.parse(res.toString());
-      }
-      catch (errorParse) {
-        return reject(errorParse);
-      }
-      if (json && isO(json)) {
-        let i = filePath.lastIndexOf('/');
-        if (i >= 0) filePath = filePath.substr(i + 1, filePath.length - i - 1 - 5);
-        i = filePath.indexOf('=');
-        filePath = [filePath.substr(0, i), filePath.substr(i + 1).replace('-', '/')];
-        return resolve([filePath, json]);
-      }
-      return reject(new Error('Bad json'));
-    });
-  });
-});
-
-const getDDocs = (ddocs) => {
-  if (!(ddocs && isA(ddocs) && ddocs.length > 0)) return Promise.resolve([]);
-  return Promise.map(ddocs, getDDoc);
 };
 
+// create db
+const createDB = (dbName) => {
+  console.log('Create db');
+  console.log('>', dbName);
+
+  return dbQuery(dbName, 'PUT').then(res => {
+    if (res && res.ok === true) return true;
+    throw new Error('Error on create db '+ dbName + (res && res.error && res.reason ? ' ' + res.reason : ''));
+  });
+};
+
+// save doc
+const saveDoc = ([[db, name], json]) => {
+  console.log('Save doc');
+  console.log('>', db, name);
+
+  const docPath = db +'/'+ name;
+
+  return checkDB(db)
+    .catch(() => createDB(db))
+    .then(() => dbQuery(docPath))
+    .then(doc => {
+      if (doc && doc._id && doc._rev) json._rev = doc._rev;
+      return dbQuery(docPath, 'PUT', json);
+    })
+    .then(res => ['SAVED document "' + db +'/'+ name +'"', res && res.ok === true]);
+};
+
+// check main params
 const checkParams = () => new Promise((resolve, reject) => {
   const rewrite = {
     couchbox: {}
@@ -164,81 +189,36 @@ const checkParams = () => new Promise((resolve, reject) => {
   if (CORS && CORS.length > 0) {
     rewrite.cors =  { origins: CORS.join(', ') };
   }
+
   if (NODE_NAME) {
-    if (!(isS(NODE_NAME) && NODE_NAME.length > 0)) {
+    if (isS(NODE_NAME) && NODE_NAME.length > 0) {
+      rewrite.couchbox.nodename = NODE_NAME;
+    } else {
       return reject(new Error('Bad node name'));
     }
-    rewrite.couchbox.nodename = NODE_NAME;
   }
+
   if (SECRET) {
-    if (!(isS(SECRET) && SECRET.length === 32)) {
+    if (isS(SECRET) && SECRET.length === 32) {
+      rewrite.couch_httpd_auth =  { secret: SECRET };
+    } else {
       return reject(new Error('Bad secret'));
     }
-    rewrite.couch_httpd_auth =  { secret: SECRET };
   }
+
   if (REDIS_PASS) {
-    if (!(isS(REDIS_PASS) && REDIS_PASS.length > 0)) {
+    if (isS(REDIS_PASS) && REDIS_PASS.length > 0) {
+      rewrite.couchbox.redis_password = REDIS_PASS;
+    } else {
       return reject(new Error('Bad redis password'));
     }
-    rewrite.couchbox.redis_password = REDIS_PASS;
   }
 
   resolve(rewrite);
 });
 
 
-const checkDB = (dbName) => {
-  return fetch(COUCHDB_URL +'/'+ dbName, { method: 'HEAD' })
-    .then(res => {
-      if (res && res.status === 200) return true;
-      throw new Error('No db');
-    });
-};
-
-const createDB = (dbName) => {
-  return fetch(COUCHDB_URL +'/'+ dbName, {
-    method: 'PUT',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json; charset=utf-8',
-    }
-  }).then(res => res.json())
-    .then(res => res && res.ok === true);
-};
-
-
-const saveDDoc = ([[db, name], json]) => checkDB(db).catch(() => createDB(db)).then(() => {
-  return fetch(COUCHDB_URL +'/'+ db +'/'+ name, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    body: JSON.stringify(json)
-  }).then(res => res.json()).then(res => {
-
-    if (res && res._id && res._rev) {
-      json._rev = res._rev;
-    }
-
-    return fetch(COUCHDB_URL +'/'+ db +'/'+ name, {
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify(json)
-    }).then(res => res.json()).then(res => [[db, name], res && res.ok === true]);
-  });
-});
-
-
-Promise.all([
-  checkParams(),
-  dbQuery('/_config'),
-  getConfigFile(CONFIG_PATH),
-  getDDocs(DDOCS)
-]).then(([params, conf, json, ddocs]) => {
+const onSetup = ([params, conf, json, docs]) => {
   const save_actions = [];
   const remove_actions = [];
 
@@ -248,13 +228,31 @@ Promise.all([
   const c = Object.extended({}).merge(json).merge(params, true);
   Object.keys(c).forEach(k1 => Object.keys(c[k1]).forEach(k2 => save_actions.push([k1 +'/'+ k2, c[k1][k2]])));
 
-  const patchConfig = () => Promise.map(save_actions, args => saveConfigItem.apply(this, args), {concurrency: 1})
-    .then(res => {
-      res.forEach(row => console.log(row.join('\n= ')));
-      return Promise.map(ddocs, saveDDoc).then(res => res.forEach(row => console.log(row)));
-    });
 
-  if (MODE === MODE_PATCH) return patchConfig();
-  return Promise.map(remove_actions, args => removeConfigItem.apply(this, args), { concurrency: 1 }).then(patchConfig);
-})
+  const onResult = res => res.forEach(row => console.log(row.join(' = ')));
+
+
+  const cleanConfig = () => Promise.map(remove_actions, args => removeConfigItem.apply(this, args), { concurrency: 1 });
+  const patchConfig = () => Promise.map(save_actions, args => saveConfigItem.apply(this, args), { concurrency: 1 }).then(onResult);
+  const updateDocs = () => Promise.map(docs, saveDoc).then(onResult);
+
+
+  if (MODE === MODE_PATCH) {
+    return patchConfig().then(updateDocs);
+  } else {
+    return cleanConfig().then(patchConfig).then(updateDocs);
+  }
+};
+
+
+// validate params, load couchbox.json & docs
+const tasks = [
+  checkParams(),
+  dbQuery('/_config'),
+  getConfigFile(CONFIG_PATH),
+  getDocs(DOCS)
+];
+
+Promise.all(tasks)
+  .then(onSetup)
   .catch(err => console.error(err));
